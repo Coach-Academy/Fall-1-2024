b4b4c52dbcf4e86c71b2402026eeefcc9146d6da
abdualla1 <38115506+abdualla1@users.noreply.github.com>
2024-09-23 23:59:00 +0300

    LEcture week 2

---
+++ Level 1/CPU - Standard - L1 - C107/Lecture/Week 02/A1.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    char c;
+    cin >> c;
+    switch(c){
+        case 'a' : {
+            cout << "vowel" << '\n';
+            break;
+        }
+        case 'e' : {
+            cout << "vowel" << '\n';
+            break;
+        }
+        case 'i' : {
+            cout << "vowel" << '\n';
+            break;
+        }
+        case 'o' : {
+            cout << "vowel" << '\n';
+            break;
+        }
+        case 'u' : {
+            cout << "vowel" << '\n';
+            break;
+        }
+        default:
+            cout << "consonant" << '\n';
+    }
+    return 0;
+}
