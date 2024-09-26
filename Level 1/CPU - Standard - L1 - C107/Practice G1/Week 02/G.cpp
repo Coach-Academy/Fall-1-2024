35a03bb981d7b47e7d0b136dfafaf34852215c6e
Omar_Alaraby <omar.alaraby23@gmail.com>
2024-09-26 21:39:09 +0300

    Practice Problems L1 standard G2 Week #2

---
+++ Level 1/CPU - Standard - L1 - C107/Practice G1/Week 02/G.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int32_t main(){
+
+    int a, b;
+    cin >> a >> b;
+
+    if (b >= a) {
+        cout << a;
+    }
+    else {
+        cout << a - 1;
+    }
+
+    return 0;
+}
